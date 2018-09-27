e35d3b7448aaa94d8bfbf18de5d050203f733b18
Simon Mbuyi <simonmwepu@gmail.com>
2018-09-27 20:40:09 +0200

    [ADD] creation de la methode info

--- application/controllers/info_resto.php
+++ application/controllers/info_resto.php
@@ -1,3 +1,23 @@
 <?php
+	class Info_resto extends CI_Controller{
 
+		public function __construct (){
+
+			parent::__construct();
+
+			$this->load->database();
+		}
+
+		public function index(){
+
+			$this->info();
+		}
+
+		public function info(){
+
+			$this->load->model("info_resto_dao", "inf_dao");
+			$data["info_resto"] = $this->info_resto_dao->info_resto();
+			$this->load->view("vue_resto", $data);
+		}
+	}
 ?>
