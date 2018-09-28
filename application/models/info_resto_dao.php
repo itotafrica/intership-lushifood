82e6242f5d66f1ad55f5a64b4e343a1b87be090a
Simon Mbuyi <simonmwepu@gmail.com>
2018-09-28 18:11:38 +0200

    [UPDATE] reglage de l'erreur sur (int)$id qui etait int($id)

--- application/models/info_resto_dao.php
+++ application/models/info_resto_dao.php
@@ -1,10 +1,12 @@
 <?php 
 	class Info_resto_dao extends CI_Model{
 
-		public function info_resto(){
+		private $table = "restaurant";
+
+		public function info_resto($id){
 
 			$requete = $this->db->select(Array('nom','adresse','phone','mail'))
-								->WHERE('id',int($id))
+								->WHERE('id',(int)$id)
 								->get($this->table);
 			return $requete;
 		}
