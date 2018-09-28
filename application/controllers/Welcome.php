70da49bcf2cf928cbc43f725282b7cb4587e6bf9
Roland Beni <roland.beni1@gmail.com>
2018-09-28 13:59:16 +0200

    [UPDATE] methode chat() & methode inde()

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -9,18 +9,26 @@ class Welcome extends CI_Controller {
 	
 	public function index()
 	{
-		$this->load->view('chat');
+		$this->load->view('index');
 	}
+
 	public function link_detail(){
 		$this->load->view('product-detail');
 	}
+
 	public function link_about(){
 		$this->load->view('about');
 	}
+
 	public function link_login(){
 		$this->load->view('login');
 	}
+	
 	public function link_restau(){
 		$this->load->view('restaurant-page');
+	}
+	
+	public function chat(){
+		$this->load->view('chat');
 	}
 }
