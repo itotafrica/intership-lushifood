b37599c430395e0f05f39103f9adfddd503dd912
elvis kankola <elviskankola1@gmail.com>
2018-09-28 10:21:21 +0200

    modification  du model

--- application/controllers/panier_and_cmd.php
+++ application/controllers/panier_and_cmd.php
@@ -3,6 +3,10 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class paniercmd extends CI_Controller {
 
+    function __construct(){
+        parent::__construct();
+    }
+    
     public function add_in_panier(){
 
             $data = array('id' => $this->input->post('id'),
@@ -41,21 +45,16 @@ class paniercmd extends CI_Controller {
     //==========================================================================================================
         public function passetacommande(){
             $nomuser = strip_tags($this->input->post('nameuser'));
-            $teluser = strip_tags($this->input->post('number'));
             $addressemailuser = strip_tags($this->input->post('mail'));
-            $addressehomeuser = strip_tags($this->input->post('adresse'));
             $nomarticle = $this->input->post('nomarticle');
             $prixarticle = $this->input->post('prixarticle');
-            $taillearticle = strip_tags($this->input->post('taillearticle'));
-            $couleurarticle =  strip_tags($this->input->post('colorarticle'));
             $quantity = strip_tags($this->input->post('quantity'));
             if (empty($nomuser) OR empty($teluser) OR empty($addressemailuser) OR empty($addressehomeuser)) {
                 redirect($_SERVER['HTTP_REFERER']);
             }
-            if(!$this->seach->writecorrectynumberphone($teluser) OR !$this->seach->writecorrectyadressmail(	$addressemailuser)){
+            if( !$this->seach->writecorrectyadressmail(	$addressemailuser)){
                 return false;
             }	
-            $this->unique_model->add_query_commande($nomarticle,$prixarticle,$taillearticle,$couleurarticle,$quantity,$nomuser,$teluser,$addressemailuser,$addressehomeuser);
-                $this->load->view('succescmd');
+           //requete du model
         }
 }
