6d1e9295f9c33a964bfd13a8556ef0ea526c2078
Simon Mbuyi <simonmwepu@gmail.com>
2018-09-29 14:51:50 +0200

    [ADD] ajout d'insertion de lien d'image dans la bdd

--- application/controllers/info_resto.php
+++ application/controllers/info_resto.php
@@ -26,15 +26,39 @@
 		}
 		
 		public function add_items(){
-			
-			$nom = $this->input->post('nom');
-			$description = $this->input->post('description');
-			$pu = $this->input->post('pu');
-		
-			$data['get_items'] = $this->info_resto_dao->get_items($nom, $description, $pu);
-			$this->load->view('vue_items_info', $data);
-			
+			$idresto = 1;
+			$idresto = 1;
+			$config['upload_path'] = 'assets/uploads';
+			$config['allowed_types'] = 'jpg|JPG|PNG|png';
+			$config['max_size'] = '500';
+			$config['max_width'] = '1024';
+			$config['max_height'] = '768';
 
+			$this->load->library('upload', $config);
+
+			if( ! $this->upload->do_upload()){
+
+				$error = array('error' => $this->upload->display_errors() );
+				$this->load->view('vue_items_info', $error);
+			}
+			else{
+
+				$nom = $this->input->post('nom');
+				$description = $this->input->post('description');
+				$pu = $this->input->post('pu');
+				
+				$chemin = $this->upload->data();
+				$data = array('upload_data' => $this->upload->data());
+				foreach($chemin as $key => $value){
+					if($key==='file_name'){
+						$file='./assets/uploads/'.$value;
+					}
+				}
+
+			}
+
+			$data['get_items'] = $this->info_resto_dao->get_items($nom, $description, $pu, $idcot, $idresto, $image);
+			$this->load->view('vue_items_info', $data);
 		}
 
 		public function page(){
@@ -43,6 +67,7 @@
 		}
 
 		public function update_items(){
+
 			
 		}
 	}
