a567ccee3ad8923a3efd2be81a889a5458fdcec1
Simon Mbuyi <simonmwepu@gmail.com>
2018-09-29 11:44:32 +0200

    [UPDATE] remise de index.php

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/GLOU/Lushi-Food-Web';
+$config['base_url'] = 'http://localhost:85/GLOU/Lushi-Food-Web';
 
 /*
 |--------------------------------------------------------------------------
@@ -35,7 +35,7 @@ $config['base_url'] = 'http://localhost/GLOU/Lushi-Food-Web';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = '';
+$config['index_page'] = 'index.php';
 
 /*
 |--------------------------------------------------------------------------
