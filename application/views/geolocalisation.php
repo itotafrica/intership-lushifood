166a4d20528007ff91f7580cd9bd7a4aefa78af7
JoMasterKal <16KM110@esisalama.org>
2018-09-29 16:10:33 +0200

    appelle de la carte

---
+++ application/views/geolocalisation.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+	<head>
+        <meta charset="utf-8">
+        <!-- Nous chargeons les fichiers CDN de Leaflet. Le CSS AVANT le JS -->
+        <link rel="stylesheet" href="https://unpkg.com/leaflet@1.3.1/dist/leaflet.css" integrity="sha512-Rksm5RenBEKSKFjgI3a41vrjkw4EVPlJ3+OiI65vTjIdo9brlAacEuKOiQ5OFh7cOI1bkDwLqdLw3Zg0cRJAAQ=="
+            crossorigin="" />
+        <script src="https://unpkg.com/leaflet@1.3.1/dist/leaflet.js" integrity="sha512-/Nsx9X4HebavoBvEBuyp3I7od5tA0UzAxs+j83KgC8PU0kgB4XiK4Lfe4y4cgBtaRJQEIFCW+oC506aPT2L1zw=="
+            crossorigin=""></script>
+		<script type="text/javascript" scr ="js/goeloc.js?id=<?php echo time(); ?>"></script>
+                
+	</head>
+	<body>
+		
+        <div id="map" style = "width : 640px; height :480px;">
+			<!-- Ici s'affichera la carte -->
+		</div>
+	</body>
+</html><?php
+
+?>
