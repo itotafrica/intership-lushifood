0ad16f9d8ae1370de788192acfaf0b832c4b1dbb
JoMasterKal <16KM110@esisalama.org>
2018-09-30 00:32:19 +0200

    tous les js sur la page meme page

--- application/views/geolocalisation.php
+++ application/views/geolocalisation.php
@@ -7,15 +7,38 @@
             crossorigin="" />
         <script src="https://unpkg.com/leaflet@1.3.1/dist/leaflet.js" integrity="sha512-/Nsx9X4HebavoBvEBuyp3I7od5tA0UzAxs+j83KgC8PU0kgB4XiK4Lfe4y4cgBtaRJQEIFCW+oC506aPT2L1zw=="
             crossorigin=""></script>
-		<script type="text/javascript" scr ="../../assets/carte/goeloc.js?id=<?php echo time(); ?>"></script>
-                
+            <script type="text/javascript">
+			// On initialise la latitude et la longitude de Paris (centre de la carte)
+			var lat = 48.852969;
+			var lon = 2.349903;
+			var macarte = null;
+			// Fonction d'initialisation de la carte
+			function initMap() {
+				// Créer l'objet "macarte" et l'insèrer dans l'élément HTML qui a l'ID "map"
+                macarte = L.map('map').setView([lat, lon], 11);
+                // Leaflet ne récupère pas les cartes (tiles) sur un serveur par défaut. Nous devons lui préciser où nous souhaitons les récupérer. Ici, openstreetmap.fr
+                L.tileLayer('https://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png', {
+                    // Il est toujours bien de laisser le lien vers la source des données
+                    attribution: 'données © <a href="//osm.org/copyright">OpenStreetMap</a>/ODbL - rendu <a href="//openstreetmap.fr">OSM France</a>',
+                    minZoom: 1,
+                    maxZoom: 20
+                }).addTo(macarte);
+            }
+			window.onload = function(){
+				// Fonction d'initialisation qui s'exécute lorsque le DOM est chargé
+				initMap(); 
+			};
+		</script>
+		<style type="text/css">
+			#map{ /* la carte DOIT avoir une hauteur sinon elle n'apparaît pas */
+				height:400px;
+			}
+		</style>
+		<title>Carte</title>
 	</head>
 	<body>
-		
-        <div id="map" style = "width : 640px; height :480px;">
+		<div id="map">
 			<!-- Ici s'affichera la carte -->
 		</div>
 	</body>
-</html><?php
-
-?>
+</html>
