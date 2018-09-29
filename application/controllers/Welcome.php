0e80c7134d3720365c7af43397873f646e97ef4a
elvis kankola <elviskankola1@gmail.com>
2018-09-29 12:03:30 +0200

    [UPDATE]

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -9,7 +9,7 @@ class Welcome extends CI_Controller {
 	
 	public function index()
 	{
-		$this->load->view('panier');
+		$this->load->view('restaurant-page');
 	}
 	public function pagedetailplat(){
 		$this->load->view('product-detail');
