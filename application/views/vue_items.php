aecf89559522ba4cfcc9f03208b877052e399f54
Simon Mbuyi <simonmwepu@gmail.com>
2018-10-01 15:34:52 +0200

    [UPDATE] modification de info_resto par resto

--- application/views/vue_items.php
+++ application/views/vue_items.php
@@ -7,9 +7,8 @@
 	<title>Items</title>
 </head>
 <body>
-
-	<?php echo form_open_multipart('info_resto/add_items'); ?>
-
+	<?php echo form_open_multipart('resto/add_items'); ?>
+	<?php //echo $error;?>
 		<label for="nom" name="nom" >Nom</label>
 		<input type="text" for="nom" name="nom">
 
@@ -19,8 +18,8 @@
 		<label for="pu" name="pu" >pu</label>
 		<input type="text" name="pu">
 
-		<label for="image" name="image" >pu</label>
-		<input type="file" name="image">
+		<label for="image" name="image" >image</label>
+		<input type="file" name="image"  size="20" >
 
 		<input type="submit" value ="envoyer">
 	<?php echo form_close(); ?>
