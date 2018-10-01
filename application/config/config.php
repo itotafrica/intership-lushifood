395047c7c85cb085783f7fa13882e9483531d3d6
Simon Mbuyi <simonmwepu@gmail.com>
2018-10-01 12:59:44 +0200

    [UPDATE] chanement de chemin

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost:85/GLOU/Lushi-Food-Web';
+$config['base_url'] = 'http://localhost:85/Lushi-Food-Web';
 
 /*
 |--------------------------------------------------------------------------
