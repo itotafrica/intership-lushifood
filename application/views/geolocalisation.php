003f05d078a0463777e56e52800a90e6f07ac24d
JoMasterKal <16KM110@esisalama.org>
2018-10-01 15:08:09 +0200

    affichage de la carte....

--- application/views/geolocalisation.php
+++ application/views/geolocalisation.php
@@ -21,9 +21,6 @@
     			var lat= position.coords.latitude;
     			var long=position.coords.longitude
     			var macarte = null;
-			// Fonction d'initialisation de la carte
-			function initMap() {
-				// Créer l'objet "macarte" et l'insèrer dans l'élément HTML qui a l'ID "map"
                 macarte = L.map('map').setView([lat, lon], 11);
                 // Leaflet ne récupère pas les cartes (tiles) sur un serveur par défaut. Nous devons lui préciser où nous souhaitons les récupérer. Ici, openstreetmap.fr
                 L.tileLayer('https://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png', {
@@ -33,23 +30,7 @@
                     maxZoom: 20
                 }).addTo(macarte);
             }
-			window.onload = function(){
-				// Fonction d'initialisation qui s'exécute lorsque le DOM est chargé
-				initMap(); 
-			};
-			function geoloc(){ // ou tout autre nom de fonction
-    			var geoSuccess = function(position) { // Ceci s'exécutera si l'utilisateur accepte la géolocalisation
-        		startPos = position;
-        		userlat = startPos.coords.latitude;
-        		userlon = startPos.coords.longitude;
-        		console.log("lat: "+userlat+" - lon: "+userlon);
-    			};
-    		var geoFail = function(){ // Ceci s'exécutera si l'utilisateur refuse la géolocalisation
-        	console.log("refus");
-    		};
-    		// La ligne ci-dessous cherche la position de l'utilisateur et déclenchera la demande d'accord
-    		navigator.geolocation.getCurrentPosition(geoSuccess,geoFail);
-			}
+			getLocation();
 			
 		</script>
 		<style type="text/css">
