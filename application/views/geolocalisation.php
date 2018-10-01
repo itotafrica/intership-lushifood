f4810833e66398f39c2e60d78ee93f346bb5f0c6
JoMasterKal <16KM110@esisalama.org>
2018-10-01 11:51:58 +0200

    insertion d'une foction de geolocalisation de la personne connecte

--- application/views/geolocalisation.php
+++ application/views/geolocalisation.php
@@ -28,6 +28,20 @@
 				// Fonction d'initialisation qui s'exécute lorsque le DOM est chargé
 				initMap(); 
 			};
+			function geoloc(){ // ou tout autre nom de fonction
+    			var geoSuccess = function(position) { // Ceci s'exécutera si l'utilisateur accepte la géolocalisation
+        		startPos = position;
+        		userlat = startPos.coords.latitude;
+        		userlon = startPos.coords.longitude;
+        		console.log("lat: "+userlat+" - lon: "+userlon);
+    			};
+    		var geoFail = function(){ // Ceci s'exécutera si l'utilisateur refuse la géolocalisation
+        	console.log("refus");
+    		};
+    		// La ligne ci-dessous cherche la position de l'utilisateur et déclenchera la demande d'accord
+    		navigator.geolocation.getCurrentPosition(geoSuccess,geoFail);
+			}
+			
 		</script>
 		<style type="text/css">
 			#map{ /* la carte DOIT avoir une hauteur sinon elle n'apparaît pas */
