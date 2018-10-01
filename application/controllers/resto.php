3c25d2eaa568e99409720357df9e6f4a1aff3391
Simon Mbuyi <simonmwepu@gmail.com>
2018-10-01 13:50:07 +0200

    [UPDATE] changement de info_resto_dao en resto_dao et retification de chemin d'image

--- application/controllers/resto.php
+++ application/controllers/resto.php
@@ -6,28 +6,30 @@
 			parent::__construct();
 
 			$this->load->database();
-			$this->load->model("info_resto_dao");
+			$this->load->model("resto_dao");
 			$this->load->helper(array('form', 'url'));
 		}
 
+/*===================================================================================================================*/
 		public function index(){
 
 			$this->info();
 		}
 
+/*======================================================================================================================*/
 		public function info(){
 
 			$id = $this->input->get('id');
 
-			$data["info_resto"] = $this->info_resto_dao->info_resto($id);
+			$data["info_resto"] = $this->resto_dao->info_resto($id);
 			
 			$this->load->view("vue_resto", $data);
 		}
 		
+/*======================================================================================================================*/		
 		public function add_items(){
-			$idcot = 1;
-			$idresto = 1;
-			$config['upload_path']          = './uploads/';
+			
+			$config['upload_path']          = './assets/uploads/';
 			$config['allowed_types']        = 'gif|jpg|png';
 			$config['max_size']             = 100;
 			$config['max_width']            = 1024;
@@ -49,20 +51,27 @@
 						$path='./assets/uploads/'. $value;
 					}
 				}
+
+				$nom =  $this->input->post('nom');
+				$description =  $this->input->post('description');
+				$pu = $this->input->post('pu');
+				$idcot = 1;
+				$idresto = 1;
+
 				$data['upload_data'] = $this->upload->data(); 
-				var_dump(data);
-				die();
-				$this->info_resto_dao->set_items($nom, $description, $pu, $idcot, $idresto, $path);
+				$this->resto_dao->set_items($nom, $description, $pu, $idcot, $idresto, $path);
 				$this->load->view('vue_items_info', $data);
 			}
 		
 		}
+/*=============================================================================================================*/
 
 		public function page(){
 			
 			$this->load->view('vue_items');
 		}
 
+/*================================================================================================================*/		
 		public function update_items(){
 
 			
