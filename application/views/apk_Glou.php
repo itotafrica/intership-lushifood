82e6c6d980a39f9010165ab184871df2f06de860
rmutikamba <rmutikamba@gmail.com.com>
2018-10-02 14:31:45 +0200

    [UPDATE]MODIFYING file chat

--- application/views/apk_Glou.php
+++ application/views/apk_Glou.php
@@ -1,54 +1,200 @@
-<!-- Header section -->
-<section id="header">
-    <div class="container">
-        <div class="row">
-            <div class="col-sm-12 col-md-5">
-                <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-                    <ol class="carousel-indicators">
-                        <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-                        <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-                        <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-                    </ol>
-                    <div class="carousel-inner">
-                        <div class="carousel-item active">
-                            <img class="d-block w-100" src="#" alt="First slide">
-                        </div>
-                        <div class="carousel-item">
-                            <img class="d-block w-100" src="#" alt="Second slide">
-                        </div>
-                        <div class="carousel-item">
-                            <img class="d-block w-100" src="#" alt="Third slide">
-                        </div>
-                    </div>
-                    <!-- <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-							<span class="carousel-control-prev-icon" aria-hidden="true"></span>
-							<span class="sr-only">Previous</span>
-						</a>
-						<a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-							<span class="carousel-control-next-icon" aria-hidden="true"></span>
-							<span class="sr-only">Next</span>
-						</a> -->
+<div class="small-top">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-4 date-sec">
+                    <div id="Date"></div>
                 </div>
-            </div>
-            <div class="col-sm-12 col-md-7 pt-4 text-sm-center text-md-left text-center">
-                <div class="header-right-contents pt-5">
-                    <img class="img-fluid mb-5" src="#" alt="Glou App" />
-                    <h1 class="text-large pt-1 mb-5">Application parfaite qui facilite votre quotidien!</h1>
-                    <p>
-                        <button type="button" class="btn btn-outline-success btn-lg px-5 text-uppercase mr-md-4 mr-sm-0 btn-rounded strong mb-3">Telecharger</button>
-                        <button type="button" class="btn btn-outline-secondary btn-lg px-5 text-uppercase btn-rounded strong mb-3">lire plus</button>
-                    </p>
-
-                    <div class="icons">
-                        <a href="#!"><i class="fa fa-apple fa-2x text-success mr-5"></i></a>
-                        <a href="#!"><i class="fa fa-android fa-2x text-success mr-5"></i></a>
-                        <a href="#!"><i class="fa fa-windows fa-2x text-success"></i></a>
+                <div class="col-lg-3 offset-lg-5">
+                    <div class="social-icon">
+                        <a target="_blank" href="#" class=" fa fa-facebook"></a>
+                        <a target="_blank" href="#" class=" fa fa-twitter"></a>
+                        <a target="_blank" href="#" class=" fa fa-google-plus"></a>
+                        <a target="_blank" href="#" class=" fa fa-linkedin"></a>
+                        <a target="_blank" href="#" class=" fa fa-youtube"></a>
+                        <a target="_blank" href="#" class=" fa fa-vimeo-square"></a>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-</section>
+    <div class="top-head left">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6 col-lg-4">
+                    <h1>Glou Chat<small>Quand tu mange ca fait GLOU</small></h1>
+                </div>
+                <div class="col-md-6 col-lg-3 ml-auto admin-bar hidden-sm-down">
+                    <nav class="nav nav-inline"> <a href="#" class="nav-link"><span class="ping"></span><i class="fa fa-envelope-o"></i></a> <a href="#" class="nav-link"><i class="fa fa-bell-o"></i></a> <a href="#" class="nav-link">i.t.o.t <img class="img-fluid rounded-circle" src="http://grafreez.com/wp-content/temp_demos/river/img/admin-bg.jpg"></a>                        </nav>
+                </div>
+            </div>
+        </div>
+    </div>
+    </header>
+    <section class="top-nav">
+        <nav class="navbar navbar-expand-lg py-0">
+            <div class="container">
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+                <div class="collapse navbar-collapse" id="exCollapsingNavbar2">
+                    <ul class="nav navbar-nav ">
+                        <li class="nav-item active"> <a class="nav-link" href="#">Acceuil <span class="sr-only">(current)</span></a> </li>
+                        <li class="nav-item"> <a class="nav-link" href="#">Restaurants</a> </li>
+
+                    </ul>
 
 
-<!-- Developed by  section -->
+                </div>
+            </div>
+        </nav>
+    </section>
+
+
+
+
+
+
+
+
+
+
+
+
+    <div class="container">
+        <h3 class=" text-center">BARCELOS</h3>
+        <div class="messaging">
+            <div class="inbox_msg">
+                <div class="inbox_people">
+                    <div class="headind_srch">
+                        <div class="recent_heading">
+                            <h4>Restaurants</h4>
+                        </div>
+
+                        <form class="ml-auto">
+                            <div class="search">
+                                <input type="text" class="form-control" maxlength="64" placeholder="chercher" />
+                                <button type="submit" class="btn btn-search"><i class="fa fa-search"></i></button>
+                            </div>
+                        </form>
+
+                    </div>
+                    <div class="inbox_chat">
+                        <div class="chat_list active_chat">
+                            <div class="chat_people">
+                                <div class="chat_img"> <img src="https://ptetutorials.com/images/user-profile.png" alt="sunil"> </div>
+                                <div class="chat_ib">
+                                    <h5> BARCELOS <span class="chat_date">Dec 25</span></h5>
+                                    <p>Test, which is a new approach to have all solutions astrology under one roof.</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="chat_list">
+                            <div class="chat_people">
+                                <div class="chat_img"> <img src="https://ptetutorials.com/images/user-profile.png" alt="sunil"> </div>
+                                <div class="chat_ib">
+                                    <h5>MIGA GELATO <span class="chat_date">Dec 25</span></h5>
+                                    <p>Test, which is a new approach to have all solutions astrology under one roof.</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="chat_list">
+                            <div class="chat_people">
+                                <div class="chat_img"> <img src="https://ptetutorials.com/images/user-profile.png" alt="sunil"> </div>
+                                <div class="chat_ib">
+                                    <h5>LA PIAZZA <span class="chat_date">Dec 25</span></h5>
+                                    <p>Test, which is a new approach to have all solutions astrology under one roof.</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="chat_list">
+                            <div class="chat_people">
+                                <div class="chat_img"> <img src="https://ptetutorials.com/images/user-profile.png" alt="sunil"> </div>
+                                <div class="chat_ib">
+                                    <h5>NAZEM <span class="chat_date">Dec 25</span></h5>
+                                    <p>Test, which is a new approach to have all solutions astrology under one roof.</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="chat_list">
+                            <div class="chat_people">
+                                <div class="chat_img"> <img src="https://ptetutorials.com/images/user-profile.png" alt="sunil"> </div>
+                                <div class="chat_ib">
+                                    <h5>HYPNOSE <span class="chat_date">Dec 25</span></h5>
+                                    <p>Test, which is a new approach to have all solutions astrology under one roof.</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="chat_list">
+                            <div class="chat_people">
+                                <div class="chat_img"> <img src="https://ptetutorials.com/images/user-profile.png" alt="sunil"> </div>
+                                <div class="chat_ib">
+                                    <h5>LA PAZZA CARREFOUR <span class="chat_date">Dec 25</span></h5>
+                                    <p>Test, which is a new approach to have all solutions astrology under one roof.</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="chat_list">
+                            <div class="chat_people">
+                                <div class="chat_img"> <img src="https://ptetutorials.com/images/user-profile.png" alt="sunil"> </div>
+                                <div class="chat_ib">
+                                    <h5>Ullys Mahone <span class="chat_date">Dec 25</span></h5>
+                                    <p>Test, which is a new approach to have all solutions astrology under one roof.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="mesgs">
+                    <div class="msg_history">
+                        <div class="incoming_msg">
+                            <div class="incoming_msg_img"> <img src="https://ptetutorials.com/images/user-profile.png" alt="sunil"> </div>
+                            <div class="received_msg">
+                                <div class="received_withd_msg">
+                                    <p>salut toi</p>
+                                    <span class="time_date"> 11:01 AM    |    June 9</span></div>
+                            </div>
+                        </div>
+                        <div class="outgoing_msg">
+                            <div class="sent_msg">
+                                <p>salut</p>
+                                <span class="time_date"> 11:01 AM    |    June 9</span> </div>
+                        </div>
+                        <div class="incoming_msg">
+                            <div class="incoming_msg_img"> <img src="https://ptetutorials.com/images/user-profile.png" alt="sunil"> </div>
+                            <div class="received_msg">
+                                <div class="received_withd_msg">
+                                    <p>tu vas bien?</p>
+                                    <span class="time_date"> 11:01 AM    |    Yesterday</span></div>
+                            </div>
+                        </div>
+                        <div class="outgoing_msg">
+                            <div class="sent_msg">
+                                <p>oui et toi?</p>
+                                <span class="time_date"> 11:01 AM    |    Today</span> </div>
+                        </div>
+                        <div class="incoming_msg">
+                            <div class="incoming_msg_img"> <img src="https://ptetutorials.com/images/user-profile.png" alt="sunil"> </div>
+                            <div class="received_msg">
+                                <div class="received_withd_msg">
+                                    <p>je vais bien aussi</p>
+                                    <span class="time_date"> 11:01 AM    |    Today</span></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="type_msg">
+                        <div class="input_msg_write">
+                            <input type="text" class="write_msg" placeholder="Ecrire un message" />
+                            <button class="msg_send_btn" type="button"><i class="fa fa-paper-plane-o" aria-hidden="true"></i></button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+            <p class="text-center top_spac"> copyright (&copy;) I.T.O.T Africa 2018 </p>
+
+        </div>
+    </div>
+</body>
+
+</html>
