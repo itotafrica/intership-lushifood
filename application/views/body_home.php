ce70f07610fe5d20395f690fd496ef730af3f002
rmutikamba <rmutikamba@gmail.com.com>
2018-10-02 13:57:50 +0200

    home file who will be called in the main file

---
+++ application/views/body_home.php
@@ -0,0 +1,198 @@
+<!--main content section-->$_COOKIE
+<section>
+    <div class="mb-5">
+        <div class="container">
+            <div class="row title py-3">
+                <h5><strong>Resultat Des Recherches Filtrees</strong></h5>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-3">
+                <div clas="row mb-3">
+                    <div class="col-md-12">
+                        <div class="card">
+                            <div classs="card-body">
+                                <strong><p>Categories</p></strong>
+                                <ul clss="list-unstyled">
+                                    <a href="#">
+                                        <li>restaurant 5 etoiles</li>
+                                    </a>
+                                    <a href="#">
+                                        <li>restaurant 4 etoiles</li>
+                                    </a>
+                                    <a href="#">
+                                        <li>restaurant 3 etoiles</li>
+                                    </a>
+                                    <a href="#">
+                                        <li>restaurant 2 etoiles</li>
+                                    </a>
+                                    <a href="#">
+                                        <li>Fast Food</li>
+                                    </a>
+                                    <a href="#">
+                                        <li>take away</li>
+                                    </a>
+
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row mb-3">
+                    <div class="col-md-12">
+                        <div class="card">
+                            <div class="card-body">
+                                <strong><p>Specialite</p></strong>
+                                <ul class="list-unstyled">
+                                    <a href="#">
+                                        <li>Congolaise</li>
+                                    </a>
+                                    <a href="#">
+                                        <li>Italienne</li>
+                                    </a>
+                                    <a href="#">
+                                        <li>Chinoise</li>
+                                    </a>
+                                    <a href="#">
+                                        <li>Indienne</li>
+                                    </a>
+                                    <a href="#">
+                                        <li>Grec</li>
+                                    </a>
+                                    <a href="#">
+                                        <li>francaise</li>
+                                    </a>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row mb-3">
+                    <div class="col-md-12">
+                        <div class="card">
+                            <div class="card-body">
+                                <strong><p>Localisation</p></strong>
+                                <ul class="list-unstyled">
+                                    <a href="#">
+                                        <li>Centre ville</li>
+                                    </a>
+                                    <a href="#">
+                                        <li>carrefour</li>
+                                    </a>
+                                    <a href="#">
+                                        <li>Golf</li>
+                                    </a>
+                                    <a href="#">
+                                        <li>Hotel</li>
+                                    </a>
+                                    <a href="#">
+                                        <li>Ferme</li>
+                                    </a>
+
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row mb-3">
+                    <div class="col-md-12">
+                        <div class="card">
+                            <div class="card-body">
+                                <p>
+                                    <h5>Cliquer pour voir les retaurants autour de vous sur la carte et choisissez le plus proche. </h5>
+                                </p>
+                                <a href="map.html"> <button type="button" class="btn btn-outline bg-orange btn-block">carte</button></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
+            <div class="col-md-9">
+                <div class="row mb-3">
+                    <div class="col-md-12">
+                        <div class="card">
+                            <div class="card-body">
+                                <div class="row">
+                                    <div class="col-md-3">
+                                        <img src="https://s3.envato.com/files/248072868/Screenshot/01_preview.jpg">
+                                    </div>
+                                    <div class="col-md-7">
+                                        <h5>MIGA GELATO</h5>
+                                        <small>Golf Inc., Lubumbashi</small>
+                                        <p><small>11907 Dyuuleves Incorpotatestion, South west, Newzealer</small></p>
+                                    </div>
+                                    <div class="col-md-2">
+                                        <button type="button" class="btn btn-outline bg-orange">Voir Menu</button>
+                                        <small>Publie en Nov 27, 2017</small>
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row mb-3">
+                    <div class="col-md-12">
+                        <div class="card">
+                            <div class="card-body">
+                                <div class="row">
+                                    <div class="col-md-3">
+                                        <img src="https://s3.envato.com/files/248072868/Screenshot/01_preview.jpg">
+                                    </div>
+                                    <div class="col-md-7">
+                                        <h5>Msexchange contributes senior UX Designer</h5>
+                                        <small>Jopitor Inc., India</small>
+                                        <p><small>11907 Dyuuleves Incorpotatestion, South west, Newzealer</small></p>
+                                    </div>
+                                    <div class="col-md-2">
+                                        <button type="button" class="btn btn-outline bg-orange">Voir Menu</button>
+                                        <small>Publie en Nov 27, 2017</small>
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row mb-3">
+                    <div class="col-md-12">
+                        <div class="card">
+                            <div class="card-body">
+                                <div class="row">
+                                    <div class="col-md-3">
+                                        <img src="https://s3.envato.com/files/248072868/Screenshot/01_preview.jpg">
+                                    </div>
+                                    <div class="col-md-7">
+                                        <h5>Msexchange contributes senior UX Designer</h5>
+                                        <small>Jopitor Inc., India</small>
+                                        <p><small>11907 Dyuuleves Incorpotatestion, South west, Newzealer</small></p>
+                                    </div>
+                                    <div class="col-md-2">
+                                        <button type="button" class="btn btn-outline bg-orange">Voir Menu</button>
+                                        <small>Publie en Nov 27, 2017</small>
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+                   
+                </div>
+                <?php include("apk_Glou.php");?>
+            </div>
+
+
+
+        </div>
+
+
+
+        <!-- Developed by  section -->
+
+    </div>
+</section>
+
+        </div>
+</section>
