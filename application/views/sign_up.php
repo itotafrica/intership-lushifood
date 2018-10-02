455e2e3685b25953cf7288ca746c5964e32b1d53
rmutikamba <rmutikamba@gmail.com.com>
2018-10-02 14:40:19 +0200

    [ADD ]sign up file

---
+++ application/views/sign_up.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>inscription</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="../css/sign_up.css" />
+
+</head>
+
+<body id="font_body">
+
+
+
+
+
+    <!-- MDB core JavaScript -->
+
+
+    <div class="container" id="cont_form">
+        <div class="row">
+            <div class="card" id="font_form">
+                <div class="card-body">
+                    <div class="login-form">
+                        <form>
+                            <h2 id="text_form" class="text-center font-up font-bold deep-orange-text py-4">Inscription</h2>
+                            <div class="md-form">
+                                <i class="fa fa-user prefix grey-text"></i>
+                                <input type="text" id="orangeForm-name3" class="form-control">
+                                <label for="orangeForm-name3">votre Nom</label>
+                            </div>
+                            <div class="md-form">
+                                <i class="fa fa-envelope prefix grey-text"></i>
+                                <input type="text" id="orangeForm-email3" class="form-control">
+                                <label for="orangeForm-email3">Votre email</label>
+                            </div>
+                            <div class="md-form">
+                                <i class="fa fa-lock prefix grey-text"></i>
+                                <input type="password" id="orangeForm-pass3" class="form-control">
+                                <label for="orangeForm-pass3">Votre mot de passe</label>
+                            </div>
+                            <div class="text-center">
+                                <button class="btn btn-deep-orange">S'inscrire<i class="fa fa-angle-double-right pl-2" aria-hidden="true"></i></button>
+                            </div>
+                        </form>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
