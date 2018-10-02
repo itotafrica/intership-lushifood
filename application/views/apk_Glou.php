e0be5c56b881667eadcb69ee7d03a127e4f0a543
rmutikamba <rmutikamba@gmail.com.com>
2018-10-02 14:36:14 +0200

    [UPDATE]modifying  apk file information

---
+++ application/views/apk_Glou.php
@@ -0,0 +1,54 @@
+<!-- Header section -->
+<section id="header">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-12 col-md-5">
+                <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+                    <ol class="carousel-indicators">
+                        <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                        <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                        <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                    </ol>
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
+                            <img class="d-block w-100" src="#" alt="First slide">
+                        </div>
+                        <div class="carousel-item">
+                            <img class="d-block w-100" src="#" alt="Second slide">
+                        </div>
+                        <div class="carousel-item">
+                            <img class="d-block w-100" src="#" alt="Third slide">
+                        </div>
+                    </div>
+                    <!-- <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+							<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+							<span class="sr-only">Previous</span>
+						</a>
+						<a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+							<span class="carousel-control-next-icon" aria-hidden="true"></span>
+							<span class="sr-only">Next</span>
+						</a> -->
+                </div>
+            </div>
+            <div class="col-sm-12 col-md-7 pt-4 text-sm-center text-md-left text-center">
+                <div class="header-right-contents pt-5">
+                    <img class="img-fluid mb-5" src="#" alt="Glou App" />
+                    <h1 class="text-large pt-1 mb-5">Application parfaite qui facilite votre quotidien!</h1>
+                    <p>
+                        <button type="button" class="btn btn-outline-success btn-lg px-5 text-uppercase mr-md-4 mr-sm-0 btn-rounded strong mb-3">Telecharger</button>
+                        <button type="button" class="btn btn-outline-secondary btn-lg px-5 text-uppercase btn-rounded strong mb-3">lire plus</button>
+                    </p>
+
+                    <div class="icons">
+                        <a href="#!"><i class="fa fa-apple fa-2x text-success mr-5"></i></a>
+                        <a href="#!"><i class="fa fa-android fa-2x text-success mr-5"></i></a>
+                        <a href="#!"><i class="fa fa-windows fa-2x text-success"></i></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+<!-- Developed by  section -->
