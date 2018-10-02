982c8ed45a2d91176300190c873c5ab53345d221
UlysseNMK312 <16MK312@esisalama.org>
2018-10-02 13:48:30 +0000

    [UPDATE] modification de la fonction afficher_items

--- application/models/items.php
+++ application/models/items.php
@@ -9,56 +9,21 @@ class items extends CI_models
 	
 		$this->load->database();
 
-		public function afficher_items(string, $nom_rest){
-
-		if (isset($nom_rest != "") {
-		
-			$query = $this->db->query("SELRECT * FROM Items");
-
-
-			foreach ($query-> result() as $row) 
-			{
-				echo $row-> id_item;
-				echo $row-> nom_item;
-				echo $row-> descript;
-				echo $row-> prix_unit;
-				echo $row-> id_cot;
-				echo $row-> id_rest;
-				echo $row-> images;
-			}
-			$query->free_result();
+		public function afficher_items($id){
+_
+			$query = $this->db->select(Array('nom','description','prix','images'))
+								->WHERE('id',(int)$id)
+								->get($this->table)
+								->result();
+			return $requete;
 		}
-	//end_foreach();
+
 		
 		echo "les items :" .$query->num_row();
 		}
 	}
 
-	public function ajouter_items($nom_item, $descript, $id_rest, $images)
-	{
-		/*$query = $this->db->set(array(
-			'nom item' => $nom_item ,
-			'decription'=> $descript,
-			'identifiant cotation' => $id_cot,
-			'identifiant restaurant' => $id_rest,
-			'image item' => $images);
-			)*/
-			$items = new items();
-			$items->nom_item = "";
-			$items->descript = "";
-		
-			$items ->images = "";
-			$items->save();
 
-			$query = $this->db->simple_query("INSERT INTO Items VALUES($nom_item, $descript, $images)");
-
-			if ($query ==TRUE) echo "success";
-
-			else echo "Fail";
-
-
-
-	}
 
 	public function supprimer_item($id_item)
 	{
