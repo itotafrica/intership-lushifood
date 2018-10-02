253b2c8d4faf96ce148a81e88b7a34d0846a87a0
rmutikamba <rmutikamba@gmail.com.com>
2018-10-02 14:44:16 +0200

    [UPDATE]modifying file login

--- application/views/login.php
+++ application/views/login.php
@@ -1,79 +1,49 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
+
 <head>
-	<title>Login-restaurant</title>
-	<meta charset="UTF-8">
-<!--===============================================================================================-->
-	<link rel="icon" type="image/png" href="<?php echo base_url('assets\images\icons\favicon.ico');?>"/>
-<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="<?php echo base_url('assets\vendor\bootstrap\css\bootstrap.min.css');?>">
-<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href=" <?php echo base_url('assets\fonts\font-awesome-4.7.0\css\font-awesome.min.css');?>">
-<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="<?php echo base_url('assets\css\util.css');?>">
-	<link rel="stylesheet" type="text/css" href="<?php echo base_url('assets\css\main.css');?>">
-<!--===============================================================================================-->
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>login</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="../css/login.css" />
+    <link rel="stylesheet" href="../bootstrap-4.1.3-dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../font-awesome/css/all.min.css">
+
 </head>
-<body >
-	
-	<div class="limiter" >
-		<div class="container-login100" style="background-image:url('<?php echo base_url('assets/images/menu.jpg');?>');background-repeat: no-repeat;background-size: 100%;">
-			<div class="wrap-login100 p-l-50 p-r-50 p-t-77 p-b-30" style="background: rgba(255,255,255,0.8); ">
-				<form class="login100-form validate-form">
-					<span class="login100-form-title p-b-55">
-						Login
-					</span>
 
-					<div class="wrap-input100 validate-input m-b-16" data-validate = "Mettez une adresse valide svp!: exemple@email.com">
-						<input class="input100" type="text" name="email" placeholder="Adresse email">
-						<span class="focus-input100"></span>
-						<span class="symbol-input100">
-							<img src="<?php echo base_url('assets\images\user.png');?>" width="7%;">
-						</span>
-					</div>
-
-					<div class="wrap-input100 validate-input m-b-16" data-validate = "Mettez un mot de passe">
-						<input class="input100" type="password" name="pass" placeholder="Mot de passe">
-						<span class="focus-input100"></span>
-						<span class="symbol-input100">
-							<img src="<?php echo base_url('assets\images\lock.png');?>" width="7%;">
-						</span>
-					</div>
-					
-					<div class="container-login100-form-btn p-t-25">
-						<button class="login100-form-btn">
-							CONNEXION
-						</button>
-					</div>
-
-					<div class="text-center w-full p-t-42 p-b-22">
-						<span class="txt1">
-							Ou connectez-vous avec
-						</span>
-					</div>
-
-					<a href="#" class="btn-face m-b-10">
-						<i class="fa fa-facebook-official"></i>
-						Facebook
-					</a>
-
-					<a href="#" class="btn-google m-b-10">
-						<img src="<?php echo base_url('assets\images\icons\icon-google.png');?>" alt="GOOGLE">
-						Google
-					</a>
-
-				</form>
-			</div>
-		</div>
-	</div>
-	
-	
-
-	
-<!--===============================================================================================-->
-	<script src="<?php echo base_url('assets\vendor\jquery\jquery-3.2.1.min.js');?>"></script>
-<!--===============================================================================================-->
-	<script src="<?php echo base_url('assets\js\main.js');?>"></script>
-
+<body>
+    <div class="container">
+        <div class="login-form">
+            <form action="/examples/actions/confirmation.php" method="post">
+                <div class="avatar">
+                    <img src="#" alt="logo Glou" />
+                </div>
+                <h2 class="text-center">Identifiant</h2>
+                <div class="social-btn text-center">
+                    <a href="#" class="btn btn-primary btn-block btn-lg"><i class="fa fa-facebook"></i> se connecter avec <b>Facebook</b></a>
+                    <a href="#" class="btn btn-info btn-block btn-lg"><i class="fa fa-twitter"></i> se connecter avec <b>Twitter</b></a>
+                    <a href="#" class="btn btn-danger btn-block btn-lg"><i class="fa fa-google"></i> se connecter avec <b>Google</b></a>
+                </div>
+                <div class="or-seperator"><i>ou</i></div>
+                <div class="form-group">
+                    <input type="text" class="form-control" name="
+                    Nom d'utilisateur" placeholder="Nom d'utilisateur" required="required">
+                </div>
+                <div class="form-group">
+                    <input type="password" class="form-control" name="mot de passe" placeholder="Password" required="required">
+                </div>
+                <div class="form-group">
+                    <button type="submit" class="btn btn-primary btn-lg btn-block login-btn">
+                        se connecter</button>
+                </div>
+                <p class="text-center small"><a href="#">
+                    Mot de passe oublié?</a></p>
+            </form>
+            <p class="text-center small">
+                vous n'avez pas de compte? <a href="#">inscrivez-vous ici!</a></p>
+        </div>
+    </div>
 </body>
+
 </html>
