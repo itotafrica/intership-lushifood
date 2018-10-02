7e0e1c58229e59651149a129f5a1db444bd94eaa
UlysseNMK312 <16MK312@esisalama.org>
2018-10-02 10:46:41 +0000

    restau.php

---
+++ application/controllers/restau.php
@@ -0,0 +1,17 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Restau extends CI_Controller {
+
+    function __construct(){
+	
+        parent :: __construct();
+}
+}
+    public function info_restau()
+    {
+        $this->load->model('afficher_info_restau');
+    }
+
+
+?>    
