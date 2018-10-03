45721ed52a6f2636c6fd3fb3f80219567c998361
rmutikamba <rmutikamba@gmail.com.com>
2018-10-03 15:17:17 +0200

    [UPDATE]autres ajouts du caracytere "_" entre base et url

--- application/views/home.php
+++ application/views/home.php
@@ -15,16 +15,16 @@
     
     <link rel="stylesheet" href="bootstrap-4.1.3-dist/css/bootstrap.min.css" />
     <link rel="stylesheet" href="font-awesome/css/all.min.css">
-    <link rel="stylesheet" type="text/css" media="screen" href="<?php echo base url('assets\bootstrap-4.1.3-dist/css/bootstrap.min.css') ;?>" />
-    <link rel="stylesheet" type="text/css" media="screen" href="<?php echo base url('assets\font-awesome/css/all.min.css') ;?>" />
+    <link rel="stylesheet" type="text/css" media="screen" href="<?php echo base_url('assets\bootstrap-4.1.3-dist/css/bootstrap.min.css') ;?>" />
+    <link rel="stylesheet" type="text/css" media="screen" href="<?php echo base_url('assets\font-awesome/css/all.min.css') ;?>" />
 
-    <link rel="stylesheet" type="text/css" media="screen" href="<?php echo base url('assets\css\head_home.css') ;?>" />
+    <link rel="stylesheet" type="text/css" media="screen" href="<?php echo base_url('assets\css\head_home.css') ;?>" />
 
-    <link rel="stylesheet" type="text/css" media="screen" href="<?php echo base url('assets\css\chat.css');?>" />
-    <link rel="stylesheet" type="text/css" media="screen" href="<?php echo base url('assets\css\body_home.css');?>" />
+    <link rel="stylesheet" type="text/css" media="screen" href="<?php echo base_url('assets\css\chat.css');?>" />
+    <link rel="stylesheet" type="text/css" media="screen" href="<?php echo base_url('assets\css\body_home.css');?>" />
 
 
-    <link rel="stylesheet" type="text/css" media="screen" href="<?php echo base url('assets\css\apk_Glou.css');?>"/>
+    <link rel="stylesheet" type="text/css" media="screen" href="<?php echo base_url('assets\css\apk_Glou.css');?>"/>
 
     
 
