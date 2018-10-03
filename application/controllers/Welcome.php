66f38e5e4106dbb63009422977dd64445012fa14
rmutikamba <rmutikamba@gmail.com.com>
2018-10-03 16:41:44 +0200

    [UPDATE]ajout des vues dans la methode telles que le header,le footer  qui ont permis d afficher le css.

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -8,26 +8,40 @@ class Welcome extends CI_Controller {
 	}
 	
 	public function chat()
+
 	{
+		$this->load->view('globals/header');
+
 		$this->load->view('chat');
+		$this->load->view('globals/footer');
 	}
 	public function login(){
+		$this->load->view('globals/header');
 		$this->load->view('login');
+		$this->load->view('globals/footer');
+
 	}
 	public function sign_up(){
+		$this->load->view('globals/header');
+
 		$this->load->view('sign_up');
+		$this->load->view('globals/footer');
 	}
 	public function map(){
+
+		$this->load->view('globals/header');
 		$this->load->view('map');
+		$this->load->view('globals/footer');
 	}
 	
 	public function home()
 	{
-		$this->load->view('home');
+		
+		$this->load->view('globals/header');
 
 		$this->load->view('head_caroussel');
 		$this->load->view('body_home');
-		$this->load->view('footer');
+		$this->load->view('globals/footer');
 		
 	}
 
