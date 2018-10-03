1ad6540186915eaf8c5d5ebf392a0263d82648c8
rmutikamba <rmutikamba@gmail.com.com>
2018-10-03 14:52:33 +0200

    [UPDATE]ajout des lignes de codes qui servent de lien vers les les fichiers bbotstrap et jquery

--- application/views/home.php
+++ application/views/home.php
@@ -15,8 +15,10 @@
     
     <link rel="stylesheet" href="bootstrap-4.1.3-dist/css/bootstrap.min.css" />
     <link rel="stylesheet" href="font-awesome/css/all.min.css">
+    <link rel="stylesheet" type="text/css" media="screen" href="<?php echo base url('assets\bootstrap-4.1.3-dist/css/bootstrap.min.css') ;?>" />
+    <link rel="stylesheet" type="text/css" media="screen" href="<?php echo base url('assets\font-awesome/css/all.min.css') ;?>" />
 
-    <link rel="stylesheet" type="text/css" media="screen" href="<?php echo base url('assets\css\head_home.css');?>" />
+    <link rel="stylesheet" type="text/css" media="screen" href="<?php echo base url('assets\css\head_home.css') ;?>" />
 
     <link rel="stylesheet" type="text/css" media="screen" href="<?php echo base url('assets\css\chat.css');?>" />
     <link rel="stylesheet" type="text/css" media="screen" href="<?php echo base url('assets\css\body_home.css');?>" />
@@ -61,11 +63,23 @@
     
 </body>
 
-<script src="js/vendor/jquery-3.2.1.min.js"></script>
-<script src="js/vendor/popper.min.js"></script>
-<script src="bootstrap-4.1.3-dist/js/bootstrap.min.js"></script>
-<script src="jQuery/apk_Glou.js"></script>
-<script src="jQuery/head_home.js"></script>
+<script src=" <?php echo base url('assets\js\vendor\jquery-3.2.1.min.js');?>"> </script>
+
+
+<script src=" <?php echo base url('assets\bootstrap-4.1.3-dist\js\bootstrap.min.js');?> " > </script>
+
+<script src=" <?php echo base url('assets\js\vendor\popper.min.js');?>  "> </script>
+
+<script src=" <?php echo base url('assets\bootstrap-4.1.3-dist\js\bootstrap.min.js');?>  "> </script>
+
+<script src=" <?php echo base url('assets\jQuery/apk_Glou.js');?>  "> </script>
+
+<script src=" <?php echo base url('assets\jQuery/jQuery/head_home.js');?>  "> </script>
+
+
+
+
+
 
 
 </html>
