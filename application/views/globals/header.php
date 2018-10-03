2f2b97244d2c033a0af8faa30863554508b6a6ce
rmutikamba <rmutikamba@gmail.com.com>
2018-10-03 15:58:15 +0200

    [ADD]creation du fichier header  qui contiendra l entete

---
+++ application/views/globals/header.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="Fr">
+
+<head>
+    <meta charset="UTF-8">
+
+    <title>HOME</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    
+    <?php echo base_url('assets\css\style.css');?>
+   
+    <link rel="stylesheet" type="text/css" media="screen" href="<?php echo base_url('assets/bootstrap-4.1.3-dist/css/bootstrap.min.css') ;?>" />
+    <link rel="stylesheet" type="text/css" media="screen" href="<?php echo base_url('assets/font-awesome/css/all.min.css') ;?>" />
+
+    <link rel="stylesheet" type="text/css" media="screen" href="<?php echo base_url('assets/css/head_home.css') ;?>" />
+
+    <link rel="stylesheet" type="text/css" media="screen" href="<?php echo base_url('assets/css/chat.css');?>" />
+    <link rel="stylesheet" type="text/css" media="screen" href="<?php echo base_url('assets/css/body_home.css');?>" />
+
+
+    <link rel="stylesheet" type="text/css" media="screen" href="<?php echo base_url('assets/css/apk_Glou.css');?>"/>
+
+    
+
+
+
+
+    
+    
+
+<link rel="stylesheet" type="text/css" media="screen" href="<?php echo base_url('assets/css/head_home.css');?>" />
+
+<link rel="stylesheet" type="text/css" media="screen" href="<?php echo base_url('assets/css/head_caroussel.css');?>" />
+
+
+
+
+
+<link rel="stylesheet" type="text/css" media="screen" href="<?php echo base_url('assets/css/footer.css');?>" />
+    
+    
+    
+</head>
+
+<body>
