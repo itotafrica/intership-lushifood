ffb79d44d6725d3307d359f824a5fe457fb522ec
rmutikamba <rmutikamba@gmail.com.com>
2018-10-03 12:11:15 +0200

    [UPDATE]ajout du code bootstrap du pied de page

---
+++ application/views/footer.php
@@ -0,0 +1,58 @@
+<footer class="footer-distributed">
+
+			<div class="footer-left">
+
+				<h3>GLOU<span>logo</span></h3>
+
+				<p class="footer-links">
+					<a href="#">A propos</a>
+					
+					.
+					<a href="#">Nous Contacter</a>
+				</p>
+
+				<p class="footer-company-name">I.T.O.T Africa &copy; 2018</p>
+			</div>
+
+			<div class="footer-center">
+
+				<div>
+					<i class="fa fa-map-marker"></i>
+					<p><span>21 Revolution Street</span> Paris, France</p>
+				</div>
+
+				<div>
+					<i class="fa fa-phone"></i>
+					<p>+1 555 123456</p>
+				</div>
+
+				<div>
+					<i class="fa fa-envelope"></i>
+					<p><a href="mailto:support@company.com">support@company.com</a></p>
+				</div>
+
+			</div>
+
+			<div class="footer-right">
+
+				<p class="footer-company-about">
+					<span>About the company</span>
+					Lorem ipsum dolor sit amet, consectateur adispicing elit. Fusce euismod convallis velit, eu auctor lacus vehicula sit amet.
+				</p>
+
+				<div class="footer-icons">
+
+					<a href="#"><i class="fa fa-facebook"></i></a>
+					<a href="#"><i class="fa fa-twitter"></i></a>
+					
+					
+
+				</div>
+
+			</div>
+
+		</footer>
+</footer>
+</body>
+
+</html>
