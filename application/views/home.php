932649bdb96d65cca210103fe5b6a658674b9dcf
rmutikamba <rmutikamba@gmail.com.com>
2018-10-03 15:51:48 +0200

    [UPDATE]J ai coupe la partie footer  pour l envoyer dans le dossier global qui est dans les vue

--- application/views/home.php
+++ application/views/home.php
@@ -52,29 +52,3 @@
     
     
     
-
-
-
-    
-</body>
-
-<script src=" <?php echo base_url('assets/js/vendor/jquery-3.2.1.min.js');?>"> </script>
-
-
-<script src=" <?php echo base_url('assets/bootstrap-4.1.3-dist/js/bootstrap.min.js');?> " > </script>
-
-<script src=" <?php echo base_url('assets/js/vendor/popper.min.js');?>  "> </script>
-
-<script src=" <?php echo base_url('assets/bootstrap-4.1.3-dist/js/bootstrap.min.js');?>  "> </script>
-
-<script src=" <?php echo base_url('assets/jQuery/apk_Glou.js');?>  "> </script>
-
-<script src=" <?php echo base_url('assets/jQuery/jQuery/head_home.js');?>  "> </script>
-
-
-
-
-
-
-
-</html>
