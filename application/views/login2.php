6cb14319e854605c49b5e72072fad8f8626e7e79
atthie <atthie27@gmail.com>
2018-10-04 16:05:30 +0200

    [UPDATE]integration of the firebase library in login2.php

--- application/views/login2.php
+++ application/views/login2.php
@@ -5,7 +5,9 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>Page Title</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-   
+    <script src="https://www.gstatic.com/firebasejs/4.9.0/firebase.js"></script>
+    <script src="https://cdn.firebase.com/libs/firebaseui/3.1.1/firebaseui.js"></script>
+<link type="text/css" rel="stylesheet" href="https://cdn.firebase.com/libs/firebaseui/3.1.1/firebaseui.css" />
    
 </head>
 <body>
