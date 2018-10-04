780b71d39c0505afd4fb3402364543285376698f
atthie <atthie27@gmail.com>
2018-10-04 15:22:56 +0200

    [UPDATE] integration of the firebase library

--- application/views/authentification.php
+++ application/views/authentification.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>Page Title</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-   
+   <script src="https://www.gstatic.com/firebasejs/4.9.0/firebase.js"></script>
    
 </head>
 <body>
